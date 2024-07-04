74a1560f52c982794010059a73f5bfa8d9d9d1a7
namengegm <namenge.gm@mail.com>
2024-07-04 12:42:26 +0300

    edited js_config.js to include mongodb credentials

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://<namengegm>:<Giddy1987@>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<namengegm>:<Giddy1987>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<namengegm>:<Giddy1987>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<namengegm>:<Giddy1987@>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<namengegm>:<Giddy1987@>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
