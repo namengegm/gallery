3a3413d43fd195cd25ab789c105c3aa9e871bf07
namengegm <namenge.gm@mail.com>
2024-07-04 16:51:47 +0300

    edited mongo db connections

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://root:12345@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://root:12345@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://root:12345@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://root:1234@cluster0.b1qkznk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://root:1234@cluster0.b1qkznk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://root:1234@cluster0.b1qkznk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
